8898f9034a1122f190d5ec38a85460ebd9e9ff9f
mehmetozanguven <mehmetozanguven@gmail.com>
2022-03-03 18:50:32 +0300

    Disable csrf protection

--- spring-boot-jwt-example/src/main/java/com/mehmetozanguven/springbootjwtexample/security/SecurityConfiguration.java
+++ spring-boot-jwt-example/src/main/java/com/mehmetozanguven/springbootjwtexample/security/SecurityConfiguration.java
@@ -65,10 +65,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf(c -> {
-            c.ignoringAntMatchers(allowedEndpoints());
-            c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-        });
+        http.csrf().disable();
 
         http.cors().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
